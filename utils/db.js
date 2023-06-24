4ffbf4b122d40ff71ada2926090e5615f5787140
shashankPatil <shashankw7@gmail.com>
2023-06-24 09:53:04 +0530

    initial commit

---
+++ utils/db.js
@@ -0,0 +1,43 @@
+/**
+* Real Time chatting app
+* @author Shashank Tiwari
+*/
+
+/**
+* Source code by https://codeburst.io/@MichalMecinski
+* https://codeburst.io/node-js-mysql-and-promises-4c3be599909b
+*/
+
+const mysql = require('mysql');
+
+class Db {
+	constructor(config) {
+		this.connection = mysql.createPool({
+			connectionLimit: 100,
+			host: '127.0.0.1',
+			user: 'root',
+			password: 'root',
+			database: 'chat',
+			debug: false
+		});
+	}
+	query(sql, args) {
+		return new Promise((resolve, reject) => {
+			this.connection.query(sql, args, (err, rows) => {
+				if (err)
+					return reject(err);
+				resolve(rows);
+			});
+		});
+	}
+	close() {
+		return new Promise((resolve, reject) => {
+			this.connection.end(err => {
+				if (err)
+					return reject(err);
+				resolve();
+			});
+		});
+	}
+}
+module.exports = new Db();
